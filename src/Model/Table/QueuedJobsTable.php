5f5a4d454abb3fcb787801907306d27be2f05283
Italo Palanza <italo.palanza@whiterabbit.cloud>
2023-10-12 15:04:31 +0200

    call Teams

--- src/Model/Table/QueuedJobsTable.php
+++ src/Model/Table/QueuedJobsTable.php
@@ -697,11 +697,9 @@ class QueuedJobsTable extends Table {
 		];
 		$job = $this->patchEntity($job, $fields);
 
-		if (class_exists('\App\MACheAccade\MACheAccade')) {
-			$error = new \App\MACheAccade\MACheAccade();
-			$error->setMessage("Job: " . $job->job_task . " - Customer: " . $job->job_group . " - ID: " . $job->id, $failureMessage);
-			$error->send();
-		}
+		$error = new \App\MACheAccade\MACheAccade();
+		$error->setMessage("Job: " . $job->job_task . " - Customer: " . $job->job_group . " - ID: " . $job->id, $failureMessage);
+		$error->send();
 
 		return (bool)$this->save($job);
 	}
