dad045f62860a3a8710f10f8edf8538218d82763
r-da <raffaele.dellaversana@marketingautomation.it>
2024-04-19 15:52:05 +0200

    QueueProcessesTable::wakeUpWorkersByPriority

--- src/Model/Table/QueueProcessesTable.php
+++ src/Model/Table/QueueProcessesTable.php
@@ -331,7 +331,7 @@ class QueueProcessesTable extends Table {
 		foreach ($processes as $process) {
 			$pid = (int)$process->pid;
 			if ($pid > 0) {
-				posix_kill($pid, \SIGUSR1);
+				posix_kill($pid, SIGUSR1);
 			}
 		}
 	}
@@ -351,7 +351,7 @@ class QueueProcessesTable extends Table {
 			if ($process->priority === $priority) {
 				$pid = (int)$process->pid;
 				if ($pid > 0) {
-					posix_kill($pid, \SIGUSR1);
+					posix_kill($pid, SIGUSR1);
 				}
 			}
 		}
